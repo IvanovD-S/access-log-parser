21acef7bd933e561c8847183534989a8300f7e55
IvanovD-S <DSIvanov@inno.tech>
2025-11-16 18:30:21 +0300

    Добавлена логика выполнение арифметических функций над двумя введенными числами

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Курсовой проект. Задание по теме "Как компилировать и запускать Java"
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        //Курсовой проект. Задание по теме ""Синтаксис языка" и “Базовые типы данных”"
+        System.out.println("Введите первое число и нажмите ENTER: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите ENTER: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        //Сумма чисел
+        int sum = firstNumber + secondNumber;
+        //Разность чисел
+        int difference = firstNumber - secondNumber;
+        //Произведение чисел
+        int product = firstNumber * secondNumber;
+        //Частное чисел
+        double quotient = (double) firstNumber / secondNumber;
+        //Выводим на экран
+        System.out.println("Сумма чисел: "+firstNumber+" + "+secondNumber+" = "+sum);
+        System.out.println("Разность чисел: "+firstNumber+" - "+secondNumber+" = "+difference);
+        System.out.println("Произведение чисел: "+firstNumber+" * "+secondNumber+" = "+product);
+        System.out.println("Частное чисел: "+firstNumber+" / "+secondNumber+" = "+quotient);
     }
 }
