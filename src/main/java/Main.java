75a23f47bfff79069e4d56ca3416ccdefc38b29d
IvanovD-S <DSIvanov@inno.tech>
2026-01-23 18:03:07 +0300

    Исправлены ошибки форматирования кода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,26 @@
 import java.io.*;
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
         // Задание #1. Обработка исключений
-        fileName();
+        try {
+            fileName();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void fileName() {
+    public static void fileName() throws IOException {
         Scanner sc = new Scanner(System.in);
         int correctPathsCount = 0;
-        boolean lineLarge1024 = false;
         int countLine = 0;
-        double yandexBot = 0;
-        double googlebot = 0;
+        double yandexBotCount = 0;
+        double googlebotCount = 0;
+        boolean isLineMoreLengthOf = false;
         Statistics stats = new Statistics();
 
         while (true) {
@@ -46,54 +50,51 @@ public class Main {
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             }
-            try {
-                BufferedReader reader = new BufferedReader(fileReader);
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (large1024(line)) {
-                        System.out.println("Строка длиннее 1024 символов № " + (countLine + 1));
-                        lineLarge1024 = true;
-                        break;
-                    }
-                    int length = line.length();
-                    countLine++;
-                    if (line.contains("Googlebot")) {
-                        googlebot++;
-                    }
-                    if (line.contains("YandexBot")) {
-                        yandexBot++;
-                    }
 
-                    LogEntry entry = new LogEntry(line);
-                    stats.addEntry(entry);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                isLineMoreLengthOf = isLineMoreLengthOf(line, 1024);
+                if (isLineMoreLengthOf) {
+                    System.out.println("Строка длиннее 1024 символов № " + (countLine + 1));
+                    break;
                 }
-            } catch (IOException e) {
-                System.out.println("Ошибка при работе с файлом: " + e.getMessage());
-            }
+                countLine++;
+                if (line.contains("Googlebot")) {
+                    googlebotCount++;
+                }
+                if (line.contains("YandexBot")) {
+                    yandexBotCount++;
+                }
 
-            if (!lineLarge1024) {
-                double trafficRate = stats.getTrafficRate();
-                Set<String> pages = stats.getExistingPages();
-                System.out.println("Существующие страницы: " + pages);
-                Map<String, Double> osStats = stats.getOsStatistics();
-                for (Map.Entry<String, Double> entry : osStats.entrySet()) {
-                    System.out.printf("ОС: %s, Доля: %.4f%n", entry.getKey(), entry.getValue());
-                }
-                System.out.println("Количество строк в файле: " + countLine);
-                System.out.println("Количество запросов Googlebot: " + (Math.floor((googlebot / countLine) * 10000)) / 100 + "% от общего числа запросов");
-                System.out.println("Количество запросов YandexBot: " + (Math.floor((yandexBot / countLine) * 10000)) / 100 + "% от общего числа запросов");
-                System.out.println("Интенсивность трафика: " + Math.floor(trafficRate * 100) / 100 + " Кб/ч");
-            } else {
-                System.out.println("Работа с файлом прекращена из-за наличия строки длиннее 1024 символов");
+                LogEntry entry = new LogEntry(line);
+                stats.addEntry(entry);
             }
+        }
+
+        if (!isLineMoreLengthOf) {
+            double trafficRate = stats.getTrafficRate();
+            Set<String> pages = stats.getExistingPages();
+            System.out.println("Существующие страницы: " + pages);
+            Map<String, Double> osStats = stats.getOsStatistics();
+            for (Map.Entry<String, Double> entry : osStats.entrySet()) {
+                System.out.printf("ОС: %s, Доля: %.4f%n", entry.getKey(), entry.getValue());
+            }
+            System.out.println("Количество строк в файле: " + countLine);
+            double googlebotCountRequest = (Math.floor((googlebotCount / countLine) * 10000)) / 100;
+            double yandexBotCountRequest = (Math.floor((yandexBotCount / countLine) * 10000)) / 100;
+            System.out.println("Количество запросов Googlebot: " + googlebotCountRequest + "% от общего числа запросов");
+            System.out.println("Количество запросов YandexBot: " + yandexBotCountRequest + "% от общего числа запросов");
+            System.out.println("Интенсивность трафика: " + Math.floor(trafficRate * 100) / 100 + " Кб/ч");
+        } else {
+            System.out.println("Работа с файлом прекращена из-за наличия строки длиннее 1024 символов");
         }
     }
 
-    private static boolean large1024(String line) {
-        if (line.length() > 1024) {
+    private static boolean isLineMoreLengthOf(String line, int length) {
+        if (line.length() > length) {
             return true;
         }
         return false;
     }
 }
-
